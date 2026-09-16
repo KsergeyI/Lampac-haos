1a6de20fab45e4a5b54e5336d08f688bd614f029
Sergey <ksergeyivanovich@gmail.com>
2026-09-16 21:33:40 +0300

    Update run.sh

--- lampac/run.sh
+++ lampac/run.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# Persistent password
+if [ ! -f /data/passwd ]; then
+    cp /lampac/passwd /data/passwd
+fi
+cp /data/passwd /lampac/passwd
+
 # Persistent cache
 mkdir -p /data/cache
 chown -R 1000:1000 /data/cache
@@ -17,5 +23,8 @@ if [ ! -L /lampac/database ]; then
     rm -rf /lampac/database
     ln -s /data/database /lampac/database
 fi
+
+chown 1000:1000 /data/passwd
+chown 1000:1000 /lampac/passwd
 
 exec su -s /bin/sh lampac -c "/usr/share/dotnet/dotnet Core.dll"
