1e7443a38c011fcd0c7c297fd9c6b6cf4bf8e8da
Sergey <ksergeyivanovich@gmail.com>
2026-09-16 21:26:43 +0300

    Update run.sh

--- lampac/run.sh
+++ lampac/run.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
 
-chown -R 1000:1000 /lampac/data
+set -e
+
+# Persistent cache
+mkdir -p /data/cache
+chown -R 1000:1000 /data/cache
+if [ ! -L /lampac/cache ]; then
+    rm -rf /lampac/cache
+    ln -s /data/cache /lampac/cache
+fi
+
+# Persistent database
+mkdir -p /data/database
+chown -R 1000:1000 /data/database
+if [ ! -L /lampac/database ]; then
+    rm -rf /lampac/database
+    ln -s /data/database /lampac/database
+fi
 
 exec /usr/share/dotnet/dotnet Core.dll
